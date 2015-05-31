11cbc3231b128d6083cad313d5a526235aa0dd83
runningtalus <dave@davemay.org>
2015-05-31 13:11:43 -0600

    Configuring the Root JS namespace at spa.js and setting JSLint switches.

--- 3/spa/js/spa.js
+++ 3/spa/js/spa.js
@@ -1,9 +1,15 @@
-/*jslint         browser : true, continue: true,
-  devel  : true, indent  : 2,    maxerr  : 50,
-  newcap : true, nomen   : true, vars    : true,
+/*
+* spa.js
+* Root namespace module
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,      maxerr : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true,     vars : false,
   white  : true
  */
-/*global jQuery */
+/*global $, spa */
 
 // Module /spa/
 // Provides chat slider capability
