17ff8f4e8683377a5b41649694490b4fed8a2423
runningtalus <dave@davemay.org>
2015-05-31 15:28:55 -0600

    Create spa.shell.js. When the shell is initialized it will fill the page element with feature containers.

---
+++ 3/spa/js/spa.shell.js
@@ -0,0 +1,16 @@
+/*
+ * spa.shell.js
+ * Shell module for SPA
+ */
+
+/*jslint         browser : true, continue : true,
+ devel  : true, indent  : 2,      maxerr : 50,
+ newcap : true, nomen   : true, plusplus : true,
+ regexp : true, sloppy  : true,     vars : false,
+ white  : true
+ */
+/*global $, spa */
+
+spa.shell = (function (){
+
+})();
