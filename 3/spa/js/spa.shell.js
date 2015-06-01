c3cf887e27bdeadc489fd4e948f9e0ab7024634a
runningtalus <dave@davemay.org>
2015-05-31 19:14:23 -0600

    Working on building in chat slider.  Stored chat sizes and time parameters as variables in the Module Scope Variable configMap object.

--- 3/spa/js/spa.shell.js
+++ 3/spa/js/spa.shell.js
@@ -28,11 +28,15 @@ spa.shell = (function () {
       + '<div class="spa-shell-foot"></div>'
       + '<div class="spa-shell-chat"></div>'
       + '<div class="spa-shell-modal"></div>'
+      chat_extend_time : 1000,
+      chat_retract_time : 300,
+      chat_extend_height : 450,
+      chat_retract_height : 15
     },
     stateMap = { $container : null },
     jqueryMap = {},
 
-    setJqueryMap, initModule;
+    setJqueryMap, toggleChat, initModule;
 //-----------END MODULE SCOPE VARIABLES----------
 
 //----------BEGIN UTILITY METHODS----------
@@ -46,6 +50,14 @@ setJqueryMap = function () {
   jqueryMap = { $container : $container };
   };
 // End DOM method /setJqueryMap/
+
+//Begin DOM method /toggleChat/
+//Purpose   :
+//Settings  :
+//Arguments :
+//Returns   :
+  toggleChat = function(){};
+//End DOM method /toggleChat/
 //----------END DOM METHODS----------
 
 //----------BEGIN EVENT HANDLERS----------
