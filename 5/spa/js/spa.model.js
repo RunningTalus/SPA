7a81ae81a97cbe2cd3a7ce3df3058aaafdd3e5e8
runningtalus <dave@davemay.org>
2015-06-22 21:55:46 -0600

    Created documentation for the People Object API.

--- 5/spa/js/spa.model.js
+++ 5/spa/js/spa.model.js
@@ -25,6 +25,48 @@ spa.model = (function () {
 
    personProto, makePerson, people, initModule;
 
+ // The people object API
+ // ---------------------
+ // The people object is available at spa.model.people.
+ // The people object provides methods and events to manage
+ // a collection of person objects. Its public methods include:
+ //
+ //   * get_user() - return the current user person object.
+ //     If the current user is not signed-in, an anonymous person
+ //     object is returned.
+ //   * get_db() - return the TaffyDB database of all the person
+ //     objects - including the current user - presorted.
+ //   * get_by_cid( <client_id> ) - return a person object with
+ //     provided unique id.
+ //   * login( <user_name> ) - login as the user with the provided
+ //     user name. The current user object is changed to reflect
+ //     the new identity. Successful completion of login
+ //     publishes a 'spa-login' global custom event.
+ //   * logout()- revert the current user object to anonymous.
+ //     This method publishes a 'spa-logout' global custom event.
+ //
+ // jQuery global custom events published by the object include:
+ //   * spa-login - This is published when a user login process
+ //     completes. The updated user object is provided as data.
+ //   * spa-logout - This is published when a logout completes.
+ //     The former user object is provided as data.
+ //
+ // Each person is represented by a person object.
+ // Person objects provide the following methods:
+ //   * get_is_user() - return true if object is the current user
+ //   * get_is_anon() - return true if object is anonymous
+ //
+ // The attributes for a person object include:
+ //   * cid - string client id. This is always defined, and
+ //     is only different from the id attribute
+ //     if the client data is not synced with the backend.
+ //   * id - the unique id. This may be undefined if the
+ //     object is not synced with the backend.
+ //   * name - the string name of the user.
+ //   * css_map - a map of attributes used for avatar
+ //     presentation.
+ //
+
  personProto = {
   get_is_user : function () {
    return this.cid === stateMap.user.cid;
